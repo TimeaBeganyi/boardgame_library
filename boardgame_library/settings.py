a211186ef9459b8072b3b484ed485c399f59459b
Timea <timeabeganyi@gmail.com>
2022-09-20 19:37:23 +0300

    setup heroku

--- boardgame_library/settings.py
+++ boardgame_library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,3 +158,5 @@ EMAIL_HOST_USER = 'ro28@horiascurtu.ro'
 EMAIL_HOST_PASSWORD = 'Django2022'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
