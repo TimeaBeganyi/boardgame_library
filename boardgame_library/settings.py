26701ad01bebf2330ce9415b42711786045be121
Timea <timeabeganyi@gmail.com>
2022-09-20 19:34:34 +0300

    heroku setup

--- boardgame_library/settings.py
+++ boardgame_library/settings.py
@@ -35,9 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'welcome',
     'boardgames',
     'userextend',
+    'django_jsonform',
+    'welcome',
 ]
 
 MIDDLEWARE = [
